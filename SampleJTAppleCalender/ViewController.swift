7a32099dfb01851bab3758ebd2021665f6511f25
Shunsuke Igusa <shunsuke.igusa@hachidoriinc.com>
2019-10-31 12:40:37 +0900

    Initial Commit

---
+++ SampleJTAppleCalender/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleJTAppleCalender
+//
+//  Created by Shunsuke Igusa on 2019/10/31.
+//  Copyright © 2019 Shunsuke Igusa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
